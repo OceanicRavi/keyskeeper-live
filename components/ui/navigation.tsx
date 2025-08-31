63c3a98ac68de92d645879d7997652c78357cdc1
OceanicRavi <18ravibhatt@gmail.com>
2025-08-31 12:23:03 +1200

    Fix Duplicate Image Import Error

--- components/ui/navigation.tsx
+++ components/ui/navigation.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { useRouter, usePathname } from 'next/navigation'
 import Link from 'next/link'
-import Image from 'next/image'
 import { 
   Menu, 
   X, 
