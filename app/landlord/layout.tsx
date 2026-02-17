f09e106f4a14565096cf9ca16d99a0d2d033eb46
Ravi <18ravibhatt@gmail.com>
2026-02-17 17:31:25 +1300

    SEO optimize

---
+++ app/landlord/layout.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: 'Property Management for Landlords | Keyskeeper NZ',
+  alternates: { canonical: 'https://www.keyskeeper.co.nz/landlord' }
+}
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>
+}
